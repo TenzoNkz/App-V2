0f9d793c55eab0adde71a8833102ca6e803e066f
Tenzo Nakazato <125721690+TenzoNkz@users.noreply.github.com>
2026-09-09 02:08:27 +0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/horizontalzzz/horizoncooler/MainActivity.kt
+++ android/app/src/main/kotlin/com/horizontalzzz/horizoncooler/MainActivity.kt
@@ -3,6 +3,7 @@ package com.horizontalzzz.horizoncooler
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
+import android.provider.Settings
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -16,6 +17,12 @@ class MainActivity: FlutterActivity() {
             if (call.method == "getBatteryTemperature") {
                 val temp = getBatteryTemperature()
                 result.success(temp.toDouble())
+            } else if (call.method == "enableLocation") {
+                // MEMAKSA MUNCULNYA HALAMAN AKTIVASI GPS JIKA LOKASI MATI
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                context.startActivity(intent)
+                result.success(true)
             } else {
                 result.notImplemented()
             }
@@ -23,7 +30,8 @@ class MainActivity: FlutterActivity() {
     }
 
     private fun getBatteryTemperature(): Float {
-        val intent = context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        // Menggunakan applicationContext agar tidak pernah bocor/hilang
+        val intent = context.applicationContext.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
         val rawTemp = intent?.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1) ?: -1
         return if (rawTemp != -1) rawTemp / 10.0f else 0.0f
     }
