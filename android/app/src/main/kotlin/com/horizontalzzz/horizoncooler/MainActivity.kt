f545e3216ce74d62fd060cbf9f0f819732764b2a
Tenzo Nakazato <125721690+TenzoNkz@users.noreply.github.com>
2026-09-09 00:54:47 +0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/horizontalzzz/horizoncooler/MainActivity.kt
+++ android/app/src/main/kotlin/com/horizontalzzz/horizoncooler/MainActivity.kt
@@ -1,6 +1,30 @@
 package com.horizontalzzz.horizoncooler
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "horizon_cooler/battery_temp"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getBatteryTemperature") {
+                val temp = getBatteryTemperature()
+                result.success(temp.toDouble())
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getBatteryTemperature(): Float {
+        val intent = context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        val rawTemp = intent?.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1) ?: -1
+        return if (rawTemp != -1) rawTemp / 10.0f else 0.0f
+    }
 }
